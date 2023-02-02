705acb5ed3968f9fea5f51419e4298db91cf1b52
jarreed0 <jarreed0@gmail.com>
2023-02-02 09:41:58 -0600

    pushing code

---
+++ build.py
@@ -0,0 +1,100 @@
+#!/bin/python3
+
+# domain,cname,registrar,issued,expires,ip,ns,rectime,is_ns
+
+from bs4 import BeautifulSoup
+import csv
+import datetime
+import pydig
+import requests
+import socket
+import urllib.parse
+import whois
+
+domain = "thechamps.co"
+
+fetched = []
+
+def update_recs(data):
+    with open("recs.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
+
+def load_recs():
+    global fetched
+    with open('recs.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            fetched.append(row['domain'])
+
+def get_current_datetime():
+    return str(datetime.datetime.now())
+
+def strip(subdomain):
+ return ".".join(subdomain.split(".")[-2:])
+
+def pull(domain, is_ns):
+ global fetched
+ if domain in fetched:
+  return None
+ fetched.append(domain)
+
+ who = whois.whois(domain)
+
+ if domain != strip(domain):
+  pull(strip(domain), 0)
+
+ ns = pydig.query(domain, 'NS')
+ for n in ns:
+  pull(n, 1)
+
+ data = [
+  domain,
+  pydig.query(domain, 'A'),
+  pydig.query(domain, 'CNAME'),
+  who.registrar,
+  who.creation_date,
+  who.expiration_data,
+  pydig.query(domain, 'A'),
+  ns,
+  get_current_datetime(),
+  is_ns
+ ]
+ print(data)
+ update_recs(data)
+ #return data
+
+def get_domain_names(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    urls = [link.get('href') for link in soup.find_all('a')]
+    domain_names = []
+    for u in urls:
+        parsed_url = urllib.parse.urlparse(u)
+        domain_names.append(parsed_url.netloc)
+    return domain_names
+
+def pull_from_url(url):
+    domains = get_domain_names(url)
+    for domain in domains:
+        if domain != None and domain != '':
+            if "http" not in domain:
+                get_domain_names("http://"+domain)
+            else:
+                get_domain_names(domain)
+            pull(domain, 0)
+
+#print(pull(domain))
+load_recs()
+
+pull(domain, 0)
+pull("shadergrounds.com", 0)
+pull("reedmedia.net", 0)
+pull("indiecapsule.com", 0)
+pull("example.com", 0)
+pull("ns.freedomdns.net", 1)
+pull("ns2.example.org", 1)
+
+pull_from_url("http://reedmedia.net")
+pull_from_url("http://godotbites.com")
+pull_from_url("http://raylib.com")
---
+++ query.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import csv
+#import socket
+
+def find_ips_and_ns_for_domain(domain):
+    ips = []
+    ns = []
+    with open('recs.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['domain'] == domain:
+                ips.append(row['ip'])
+                ns.append(row['ns'])
+    print("Finding all IPs and nameservers for domain:", domain)
+    print("IPs:", ips)
+    print("Nameservers:", ns)
+
+def get_domains_and_ns_for_ip(ip):
+    domains = []
+    ns = []
+    with open('recs.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['ip'] == ip:
+                domains.append(row['domain'])
+                ns.append(row['ns'])
+    print("Finding all domains and nameservers for IP:", ip)
+    print("Domains:", domains)
+    print("Nameservers:", ns)
+
+def get_domains_for_ns(ns):
+    domains = []
+    with open('recs.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['ns'] == ns:
+                domains.append(row['domain'])
+    print("Finding all domains and nameservers for IP:", ns)
+    print("Domains:", domains)
+
+find_ips_and_ns_for_domain("example.com")
+
+get_domains_and_ns_for_ip("71.19.154.14")
+
+get_domains_for_ns("ns2.example.org")
